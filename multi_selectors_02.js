128655a401bdee9998e49a30ce5232f2e0cb3008
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 12:36:21 +0800

    update

--- multi_selectors_02.js
+++ multi_selectors_02.js
@@ -1,6 +1,5 @@
 // document.getElementsByClassName()
 
-
 // const items = document.getElementsByClassName('collection-item');
 
 // console.log(items);
@@ -8,43 +7,39 @@
 // items[1].style.color='red';
 // items[3].textContent='hello';
 
-
 // document.getElementsByTagName()
-
 // const lis = document.getElementsByTagName('li');
 // console.log(lis);
 // console.log(lis[2]);
 // lis[0].style.color='pink';
-// lis[2].textContent='Hello';
+// lis[2].textContent='welcome';
 
-// convert html collection to array
-// const lisArr = Array.from(lis);
+// How to convert the html collection to an array
+// const lisArr =Array.from(lis);
 // console.log(lisArr);
 // lisArr.reverse();
 
-
 // document.querySelectorAll()
-// return a node list
-// node list can be treated like an array (loops, methods)
+//return a node list
+//node list can be treated like an array (loops , methods)
 // not require to convert
 
 const items = document.querySelectorAll('ul.collection li.collection-item');
 
-// console.log(items);
-
-items.forEach(function(item) {
-    item.textContent = 'Hello'
+items.forEach(function(item){
+    item.textContent='Hello'
 });
 
+const liOdd =document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
 
-
-const liOdd = document.querySelectorAll('li:nth-Child(odd)');
-const liEven = document.querySelectorAll('li:nth-Child(even)');
 
 liOdd.forEach(function(li){
-    li.style.background='#333'
+    li.style.background='#333';
 })
 
-liEven.forEach(function(li){
-    li.style.background='#f4f4f4'
-})
+for(let i = 0 ; i < liEven.length ; i++){
+    liEven[i].style.background='#f4f4f4';
+}
+
+// console.log(items);
