0747a083f52e16c19a2ac5131786c7bb17ef5ed5
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 18:18:32 +0800

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-
-const title1 = document.getElementsByClassName('card-content');
-
-title1.style.background = "black";
