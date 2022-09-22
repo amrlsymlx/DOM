0a38c57340baa86bfa7556029d33757c8a9082a3
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-22 12:51:45 +0800

    Update event_bubbling_09.js

--- event_bubbling_09.js
+++ event_bubbling_09.js
@@ -1,46 +1,47 @@
-// Event bubbling
+//Event bubbling 
 
-// Event bubbling basically is bubbling up an event through DOM
-// When an event happens on a particular element in the DOM, 
-// it actually bubbles up through its parent
+//event bubbling basically is bubbling up an event through DOM
+// when an event happens on a  particular element in the Dom , 
+//it actuaally bubbles up throgh its parent
 
-// When we click on the card-title which is the task list, 
-// the event is going to bubble up through all the parent elements
+//when we click on the card-title which is the task list , 
+//the event is going to bubble up through all the parent elements
 
-// Task list element
-// document.querySelector('.card-title').addEventListener('click', function(){
-//     console.log('.card title clicked!')
+// task list element 
+// document.querySelector('.card-title').addEventListener('click',function(){
+//     console.log('card title clicked!')
 // });
 
-// Task list parent
-// document.querySelector('.card-content').addEventListener('click', function(){
-//     console.log('.card content clicked!')
+// task list parent 
+// document.querySelector('.card-content').addEventListener('click',function(){
+//     console.log('card content clicked!')
 // });
 
-// Card Content parent
-// document.querySelector('.card').addEventListener('click', function(){
-//     console.log('.card clicked!')
+//card content parent 
+// document.querySelector('.card').addEventListener('click',function(){
+//     console.log('card  clicked!')
 // });
 
-// Card parent
-// document.querySelector('.col').addEventListener('click', function(){
-//     console.log('.col clicked!')
+// card parent
+// document.querySelector('.col').addEventListener('click',function(){
+//     console.log('col  clicked!')
 // });
 
-// col parent
-// document.querySelector('.row').addEventListener('click', function(){
-//     console.log('.row clicked!')
+//col parent
+// document.querySelector('.row').addEventListener('click',function(){
+//     console.log('row  clicked!')
 // });
 
-// Event delegation
-// It is opposite to event bubbling
-// We put the event listeners on the parent element and it goes down to the child
+//event delegaation 
+//its opposit to the event bubbling 
+// we put the event listeners on the parent
+//  element and it goes down to the  child
 
 const delItem = document.querySelector('.delete-item');
 
-document.body.addEventListener('click', deleteItem);
+document.body.addEventListener('click',deleteItem);
 
-function deleteItem(e) {
+function deleteItem(e){
     // if(e.target.parentElement.className === 'delete-item secondary-content'){
     //     console.log('delete Item');
     // }
